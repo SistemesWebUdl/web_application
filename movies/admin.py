4199fb78f7acf3889c40d2ffdd4afc97e9050e78
gerard <gerar.f87@gmail.com>
2016-04-06 17:35:31 +0200

    Add data model

--- movies/admin.py
+++ movies/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+import models
 
-# Register your models here.
+admin.site.register(models.Country)
+admin.site.register(models.Company)
+admin.site.register(models.Actor)
+admin.site.register(models.Director)
+admin.site.register(models.Movie)
--- movies/models.py
+++ movies/models.py
@@ -2,4 +2,38 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Country(models.Model):
+    name = models.TextField()
+
+class Company(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    description = models.TextField(blank=True, null=True)
+    url = models.URLField(blank=True, null=True)
+    country = models.ForeignKey(Country, default=1)
+
+class Actor(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    birthday = models.DateField()
+    deathday = models.DateField(blank=True, null=True)
+    country = models.ForeignKey(Country, default=1)
+    url = models.URLField(blank=True, null=True)
+
+class Director(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    birthday = models.DateField()
+    deathday = models.DateField(blank=True, null=True)
+    country = models.ForeignKey(Country, default=1)
+    url = models.URLField(blank=True, null=True)
+
+class Movie(models.Model):
+    id = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    date = models.DateField()
+    description = models.TextField()
+    companies = models.ManyToManyField(Company)
+    countries = models.ManyToManyField(Country, default=1)
+    actors =  models.ManyToManyField(Actor)
+    directors = models.ManyToManyField(Director)
