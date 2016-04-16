f2754a92a8a76465d9faea1cff34ee169b531e51
Alba <Alba@MacBook-Air-de-Alba.local>
2016-04-16 19:25:35 +0200

    Authentification and user management added

--- web_application/urls.py
+++ web_application/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^movies/', include('movies.urls', namespace='movies')),
+    url(r'^accounts/login/$', login, name='login'),
+    url(r'^accounts/logout/$', logout, name='logout'),
 ]
